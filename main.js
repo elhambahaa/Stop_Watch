03c1f82a901e5c4284fe3f4a672f2794f9470db5
elhambahaa <125158993+elhambahaa@users.noreply.github.com>
2023-06-11 22:48:02 +0300

    main.js

--- main.js
+++ main.js
@@ -1,54 +1,65 @@
-let hr = min = sec = ms = "0" + 0;
+let hr = min = sec = ms = "0" + 0,
+starTimer,
+isTimerRunning = false;
 
-let startBtn = document.querySelector(".start");
-let stopBtn = document.querySelector(".stop");
-let resetBtn = document.querySelector(".reset");
+const startBtn = document.querySelector(".start"),
+stopBtn = document.querySelector(".stop"),
+resetBtn = document.querySelector(".reset");
 
-startBtn.addEventListener("click",start);
-stopBtn.addEventListener("click",stop);
-resetBtn.addEventListener("click",reset);
+startBtn.addEventListener("click", start);
+stopBtn.addEventListener("click", stop);
+resetBtn.addEventListener("click", reset);
 
-function start(){
-    startBtn.classList.add("active");
-    stopBtn.classList.remove("stopActive");
+function start() {
+    if (!isTimerRunning) {
+        startBtn.classList.add("active");
+        stopBtn.classList.remove("stopActive");
+        isTimerRunning = true;
 
-    starTimer = setInterval(() => {
-        ms++;
-        ms = ms < 10 ? "0" + ms : ms;
+        starTimer = setInterval(() => {
+            ms++;
+            ms = ms < 10 ? "0" + ms : ms;
 
-        if (ms == 100){
-            sec++;
-            sec = sec <10 ? "0" + sec : sec;
-            ms = "0" + 0;
-        }
-        if (sec == 60){
-            min++;
-            min = min < 10 ? "0" + min : min;
-            sec = "0" + 0; 
-        }
-        if (min == 60){
-            hr++
-            hr = hr < 10 ? "0" + hr : hr;
-            min = "0" + 0
-        }
+            if (ms == 100) {
+                sec++;
+                sec = sec < 10 ? "0" + sec : sec;
+                ms = "0" + 0;
+            }
+            if (sec == 60) {
+                min++;
+                min = min < 10 ? "0" + min : min;
+                sec = "0" + 0;
+            }
+            if (min == 60) {
+                hr++;
+                hr = hr < 10 ? "0" + hr : hr;
+                min = "0" + 0;
+            }
 
-        putValue()
-    }, 10);
+            putValue();
+        }, 10);
+    }
 }
-function stop(){
-    startBtn.classList.remove("active");
-    stopBtn.classList.add("stopActive");
-    clearInterval(starTimer);
+
+function stop() {
+    if (isTimerRunning) {
+        startBtn.classList.remove("active");
+        stopBtn.classList.add("stopActive");
+        clearInterval(starTimer);
+        isTimerRunning = false;
+    }
 }
-function reset(){
+
+function reset() {
     startBtn.classList.remove("active");
     stopBtn.classList.remove("stopActive");
     clearInterval(starTimer);
+    isTimerRunning = false;
     hr = min = sec = ms = "0" + 0;
-    putValue()
+    putValue();
 }
 
-function putValue(){
+function putValue() {
     document.querySelector(".melisecond").innerText = ms;
     document.querySelector(".seconds").innerText = sec;
     document.querySelector(".minutes").innerText = min;
